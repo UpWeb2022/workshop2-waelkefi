954cf885550edc85b53837f233aef176d6fdc60b
waelkefi <wael.kefi@esprit.tn>
2022-04-01 20:07:36 +0200

    routing

--- src/app/lis-user/lis-user.component.ts
+++ src/app/lis-user/lis-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user';
 
 @Component({
   selector: 'app-lis-user',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lis-user.component.css']
 })
 export class LisUserComponent implements OnInit {
-
+listUser : User[];
   constructor() { }
 
   ngOnInit(): void {
+this.listUser = [
+  {
+    id: 1,
+    name: 'wael',
+    phone: '27230640',
+    email: 'wael.kefi@esprit.tn',
+    job: 'teacher',
+    picture: 'string'
+  },
+  {
+    id: 1,
+    name: 'jah',
+    phone: '206425250',
+    email: 'wael.kefi@gmail.tn',
+    job: 'web developer',
+    picture: 'string'
+  },
+]
   }
+
+delete (user : User) : void{
+let i = this.listUser.indexOf(user)
+if (i!=-1) {
+  this.listUser.splice(i,1)
+}
+}
 
 }
